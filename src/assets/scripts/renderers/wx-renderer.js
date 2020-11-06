afb45e7039e92d952e089fa49a1fe7405948ba1b
imoegirl <imoegirl@outlook.com>
2020-11-06 15:09:36 +0800

    2 # as <h3>

--- src/assets/scripts/renderers/wx-renderer.js
+++ src/assets/scripts/renderers/wx-renderer.js
@@ -105,7 +105,8 @@ let WxRenderer = function (opts) {
         case 1:
           return `<h1 ${ getStyles('h1') }>${ text }</h1>`;
         case 2:
-          return `<h2 ${ getStyles('h2') }>${ text }</h2>`;
+          // return `<h2 ${ getStyles('h2') }>${ text }</h2>`;
+          return `<h3 ${ getStyles('h3') }>${ text }</h3>`;
         case 3:
           return `<h3 ${ getStyles('h3') }>${ text }</h3>`;
         default:
